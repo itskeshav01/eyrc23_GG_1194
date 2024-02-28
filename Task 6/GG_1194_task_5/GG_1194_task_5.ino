8d0b751bf7422d69cfceb5520b5ea9d80c1008d1
itsashishrath <anandashish654@gmail.com>
2024-02-28 12:17:32 +0530

    Added Task 6 bot code, no ML

--- Task 6/GG_1194_task_5/GG_1194_task_5.ino
+++ Task 6/GG_1194_task_5/GG_1194_task_5.ino
@@ -1,10 +1,36 @@
-//ESP32 Module programming
+▪ /*Team Id: GG_1194
+▪ * Author List: Ashish Rathore, Keshav Joshi, Disha Chhabra
+▪ * Filename: Task_6 Bot_code
+▪ * Theme: <Eyrc Geo Guide (GG)>
+▪ * Functions: <determineMotion, determinePosition, stopCondition, moveForwardPWM>
+▪ * Global Variables: <
+Motion, prevNode, currentNode, nextNode, isTurning, turnNow, pathIndex, lastIndex, durationInSeconds, startTime, 
+currentTime, interval, currentState, detected, moveOnLine, boxPathIndex, currentPathIndex, numRows, sensorActiveCount, 
+isBotReached, changeState, stopIf, currentPath>
+▪ */
 
-#define LEFT_SENSOR 19
-#define CENTER_SENSOR 18
+
+#include <WiFi.h>
+#define LED 2
+
+char incomingPacket[80];
+WiFiClient client;
+
+String msg = "0";
+int counter = 0;
+int countBlink=0;
+bool loopBroken = false; // Flag to indicate if the loop has been broken
+
+const char* ssid = "ASUS";                    
+const char* password =  "ASDFGHJKL";               
+const uint16_t port = 8002;
+const char * host = "192.168.110.58"; 
+
+#define SIDE_LEFT 5
+#define LEFT_SENSOR  18
+#define CENTER_SENSOR 19
 #define RIGHT_SENSOR 21
-#define SIDE_LEFT 22
-#define SIDE_RIGHT 5
+#define SIDE_RIGHT 22
 #define BUZZER 13
 #define LED_GREEN 14
 #define LED_RED 23
@@ -15,10 +41,10 @@
 #define RIGHT_MOTOR_IN4 2
 
 // PWM values for different motions
-#define FULL_SPEED 165
-#define STOP_SPEED 0
-#define TURN_SPEED 155
-#define EXTREME_TURN_SPEED 190 // Adjust this value for reduced speed during turns
+int FULL_SPEED = 230;
+int STOP_SPEED = 0;
+int TURN_SPEED = 255;
+int EXTREME_TURN_SPEED = 200; // Adjust this value for reduced speed during turns
 
 enum Detected {
   centerOfLine,
@@ -53,46 +79,11 @@ enum Motion {
   right,
   eRight,
   eLeft,
+  sideLeft,
+  sideRight,
   stop,
   aboutTurn  
 };
-
-const char* detectedStrings[] = {
-  "centerOfLine",
-  "leftOfLine",
-  "rightOfLine",
-  "node",
-  "bothSideLine",
-  "sideRightLine",
-  "sideLeftLine",
-  "allThreeLine"
-};
-
-const char* motionStrings[] = {
- "forward",
-  "left",
-  "right",
-  "eRight",
-  "eLeft",
-  "stop",
-  "aboutTurn" 
-};
-
-const char* stateStrings[] = {
-  "pathPlanning",
-  "waitingAtStart",
-  "followLine",
-  "onNode",
-  "botTurning",
-  "updatePosition",
-  "endRun",
-  "buzzerLed",
-  "reachedEventLocation",
-  "stopAtEvent",
-  "turningAround",
-  "turningAroundLine"
-};
-
 
 
 Motion movement;
@@ -119,27 +110,25 @@ bool isBotReached=false;
 bool changeState=false;
 Detected stopIf;
 
-// int storedTurningPath[12]={
-//     23,8,23,6,23,23,3,11,1,23,6,7
-//   };
+int currentPath[55];
 
-int currentPath[55] = {0, 1, 2, 3, 4, 9, 23, 4, 3, 2, 7, 11, 23, 7, 8, 1, 23, 8, 7, 6, 10, 23, 6, 3, 23, 3, 2, 1, 0, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27, 27};
-
-
-
-// int currentPath [14]= {0,1,2,3,6,5,9,10,11,7,2,1,0} ;
-
-
-void moveForward(int leftSpeed, int rightSpeed, int reverseSpeed) {
+void moveForwardPWM(int leftSpeed, int reverseLSpeed, int rightSpeed, int reverseRSpeed) {
   analogWrite(LEFT_MOTOR_IN1, leftSpeed);
-  analogWrite(LEFT_MOTOR_IN2, reverseSpeed);  // Assume LOW for forward motion
+  analogWrite(LEFT_MOTOR_IN2, reverseLSpeed);  // Assume LOW for forward motion
   analogWrite(RIGHT_MOTOR_IN3, rightSpeed);
-  analogWrite(RIGHT_MOTOR_IN4, 0);  // Assume LOW for forward motion
+  analogWrite(RIGHT_MOTOR_IN4, reverseRSpeed);  // Assume LOW for forward motion
 }
 
-
-//Map storing the nodes of 
 Motion myArray[12][12][12];
+
+
+▪ /*
+▪ * Function Name:<determineMotion>
+▪ * Input: <detect - the detected positon of bot w.r.t lines>
+▪ * Output: <motion followed for each position>
+▪ * Logic: <this function is used to make bot follow line, by sending the motion required to turn the bot to the middle line>
+▪ * Example Call: <determineMotion(leftOfLine)>
+▪ */ 
 
 Motion determineMotion(Detected detect){
 
@@ -152,22 +141,35 @@ Motion determineMotion(Detected detect){
 } else if (detect == rightOfLine) {
     moveOnLine = right;
 
-} else if (detect == sideRightLine ) {
-    moveOnLine = eLeft;
+} else if (detect == sideRightLine && (currentNode == 9 || nextNode==9)) {
+    moveOnLine = sideLeft;
+
+} else if (detect == sideRightLine) {
+    moveOnLine = left;
+
+} else if (detect == sideLeftLine && ((currentNode==4 && nextNode==9) || (currentNode==9 && nextNode==4) ))  {
+    moveOnLine = sideRight;
 
 } else if (detect == sideLeftLine ) {
-    moveOnLine = eRight;
+    moveOnLine = right;
 
-}else if (detect == node) {
+} else if (detect == node) {
     moveOnLine = stop;
     
-}else moveOnLine=moveOnLine;
+} else moveOnLine=moveOnLine;
 
  return moveOnLine;
 
 }
 
-// Function to determine detected position based on sensor readings
+▪ /*
+▪ * Function Name:<determinePosition>
+▪ * Input: <the value of all 5 IR sensors on the bot>
+▪ * Output: <the positon of bot w.r.t to lines>
+▪ * Logic: <this function is used to make detect the positon of bot w.r.t lines by using sensor values>
+▪ * Example Call: <determinePosition(!leftSensor && rightSensor)>
+▪ */ 
+
 Detected determinePosition(bool sideLeftSensor, bool leftSensor, bool centerSensor, bool rightSensor, bool sideRightSensor) {
 
   if (!sideLeftSensor && !leftSensor && centerSensor && !rightSensor && !sideRightSensor) {
@@ -210,7 +212,13 @@ Detected determinePosition(bool sideLeftSensor, bool leftSensor, bool centerSens
   return detected;
 }
 
-
+▪ /*
+▪ * Function Name:<stopCondition>
+▪ * Input: <node>
+▪ * Output: <the condition to stop>
+▪ * Logic: <this function is used to find the positon of bot with after which it has to stop so that it reaches the bot location>
+▪ * Example Call: <determinePosition(!leftSensor && rightSensor)>
+▪ */
 
 Detected stopCondition(int node) {
     if(node == 1 || node == 3 || node==11 || node==10){
@@ -231,6 +239,10 @@ Detected stopCondition(int node) {
 void setup() {
   Serial.begin(115200);
   currentState=pathPlanning;
+
+  pinMode(LED, OUTPUT);
+  digitalWrite(LED, LOW);
+  WiFi.begin(ssid, password);
 
   pinMode(LEFT_SENSOR, INPUT);
   pinMode(CENTER_SENSOR, INPUT);
@@ -262,7 +274,7 @@ myArray[1][2][7]=eRight; myArray[7][2][1]=eLeft; myArray[3][2][7]=eLeft; myArray
 myArray[6][3][4]=eRight; myArray[4][3][6]=eLeft;
 myArray[2][3][4]=forward; myArray[4][3][2]=forward; myArray[2][3][6]=eRight; myArray[6][3][2]=eLeft;
 
-myArray[3][4][5]=eRight; myArray[5][4][3]=eLeft; myArray[3][4][9]=forward; myArray[9][4][3]=forward;
+myArray[3][4][5]=eRight; myArray[5][4][3]=eLeft; myArray[3][4][9]=forward; myArray[9][4][3]=forward; myArray[5][4][9]=eRight; myArray[9][4][5]=eLeft;
 
 myArray[4][5][6]=eRight; myArray[6][5][4]=eLeft; myArray[9][5][6]=eLeft; myArray[6][5][9]=eRight;
 myArray[4][5][9]=forward; myArray[9][5][4]=forward;
@@ -282,6 +294,16 @@ myArray[9][10][11]=forward; myArray[9][10][6]=eRight; myArray[6][10][11]=eRight;
 
 myArray[7][11][8]=eRight; myArray[8][11][7]=eLeft; myArray[10][11][8]=forward; myArray[8][11][10]=forward; myArray[10][11][7]=eRight; myArray[7][11][10]=eLeft;
 
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.println("...");
+    }
+  
+    Serial.print("WiFi connected with IP: ");
+    Serial.println(WiFi.localIP());
+    currentState = pathPlanning;
+
+
 }
 
 
@@ -299,80 +321,52 @@ void loop() {
   detected = determinePosition(sideLeftSensor, leftSensor, centerSensor, rightSensor, sideRightSensor);
   moveOnLine = determineMotion(detected);
 
-  Serial.print(prevNode);
-  Serial.print(currentNode);
-  Serial.println(nextNode);
-  Serial.print("Detected: ");
-  Serial.println(detectedStrings[detected]);
-  Serial.print("Movement: ");
-  Serial.println(motionStrings[movement]);
-  Serial.print("State: ");
-  Serial.println(stateStrings[currentState]);
-
-  // State machine to control the behavior of the bot
-  // delay(1000);
+  // State machine to control the movement of the bot and changing the state
  switch(currentState) {
+    case pathPlanning  :      
+      if (!client.connect(host, port)) {
+        delay(200);
+        return;
+      }
 
-  // Start the bot when it detects the center of the line
-  case waitingAtStart :
-    movement=stop;
-    if(detected==centerOfLine){
-    digitalWrite(BUZZER, LOW);
-    digitalWrite(LED_RED, HIGH);
-    delay(1000);
-    digitalWrite(BUZZER, HIGH);
-    digitalWrite(LED_RED, LOW);
-    movement=stop;
-    currentState=pathPlanning;
-    }
+      while(1) {
+        msg = client.readStringUntil('\n');         
+        client.print("hey!");                       
+
+        if(msg[0] == '[') {
+          loopBroken = true;
+          break;
+        }          
+      }
+
+      if (loopBroken) {
+        int i = 0;
+        char tempMsg[msg.length() + 1];
+        strcpy(tempMsg, msg.c_str()); 
+        char* ptr = strtok(tempMsg, ",[] ");
+        while (ptr != NULL && i < 55) {
+          currentPath[i++] = atoi(ptr);
+          ptr = strtok(NULL, ",[] ");
+        }
+        
+        currentState = waitingAtStart;
+      }
 
     break;
 
-    case pathPlanning  :
-
-    // digitalWrite(LED_RED, HIGH);
-    // delay(500);
-    // digitalWrite(LED_RED, LOW);
-
-    // Serial.print("BoxPathIndex :");
-    // Serial.println(boxPathIndex);
-    // lastIndex=0;
-
-    // for (int i = 0; i < 10; i++) {
-    // currentPath[i] = boxPath[boxPathIndex][i];
-    // }
-
-
-    // boxPathIndex++;
-
-    // for(int i=0; i<10; i++) {
-    //   if(currentPath[i]==23) {
-    //   break;
-    //   }
-    // lastIndex++;
-    // }
-
-    // // for (int i = 0; i < 10; i++) {
-    // // Serial.print(currentPath[i]);
-    // // Serial.print(" ");
-    // // }
-    // // Serial.println();
-    // // Serial.println(lastIndex);
-
-    // // if(boxPathIndex<4){
-    // //   boxPathIndex++;
-    // //   lastIndex=0;
-    // // }
-    delay(3000);
-    currentState=followLine;
+    // Start the bot when it detects the center of the line
+  case waitingAtStart :
+    movement=stop;
+    if(detected==centerOfLine){
+      currentState=followLine;
+      movement=moveOnLine;
+    }
 
     break;
   
   case followLine :
-
     // Continue following the line
     movement=moveOnLine;
-
     if(isBotReached==true && detected==allThreeLine){
       currentState=reachedEventLocation; 
       startTime=millis();
@@ -382,18 +376,20 @@ void loop() {
       // If a node is detected, transition to the updatePosition state
       currentState=updatePosition;
     }
-
-
-    // if(pathIndex==lastIndex && detected==allThreeLine){
-    //   // If the end of the path is reached, stop the bot
-    //   movement=stop;
-    //   currentState=endRun;
-    // }
+    
+    if(detected==allThreeLine && currentPath[pathIndex]==27) {
+      //marks the end of the run if node 27 detected
+      movement=stop;
+      digitalWrite(BUZZER, LOW);
+      digitalWrite(LED_RED, HIGH);
+      currentState=endRun;
+      movement=stop;
+    }
   
     break;
 
   case updatePosition :
-    //update current position of bot
+    //At each node update current position of bot with previousNode, currentNode and nextNode
     prevNode=currentNode;
     currentNode=nextNode;
     if(currentPath[pathIndex+1] == 23){
@@ -408,23 +404,20 @@ void loop() {
 
     pathIndex++;
     currentState=onNode;
-    // digitalWrite(BUZZER, LOW);
-    // digitalWrite(LED_GREEN, HIGH);
-    // delay(500);
-    // digitalWrite(BUZZER, HIGH);
-    // digitalWrite(LED_GREEN, LOW);
-
+    startTime=millis();
     break;
   
   case onNode :
+  //find the direction of turns based on currentNode , nextNode, previousNode
+    currentTime=millis();
+    interval=currentTime-startTime;
     if(detected==node){
       movement=forward;
   } else {
     movement=moveOnLine;
-    if(detected==centerOfLine || detected==allThreeLine){
+    if((detected == centerOfLine || detected==allThreeLine) && interval >150){
       currentState=botTurning;
-
-
+      movement=myArray[prevNode][currentNode][nextNode];
     }
 
   } 
@@ -432,47 +425,58 @@ void loop() {
   break;
 
   case botTurning :
-
-    movement=myArray[prevNode][currentNode][nextNode];
-
-    if(movement==eRight && detected==rightOfLine) {
+    //move a bit forward before starting to turn for better turning accurcy
+    //turning the bot left, right, forward on reaching node
+    if(changeState==false && movement==eRight && detected==rightOfLine) {
     movement=moveOnLine;
+    startTime=millis();
     changeState=true;
 
-  } else if(movement==eLeft && detected==leftOfLine) {
+  } else if(changeState==false && movement==eLeft && detected==leftOfLine) {
     movement=moveOnLine;
+    startTime=millis();
     changeState=true;
 
-  } else if(movement==forward && detected!=node){
+  } else if(changeState==false && movement==forward && detected!=node){
     movement=moveOnLine;
+    startTime=millis();
     changeState=true;
-
   }
 
-  if(changeState==true && detected==centerOfLine){
-    currentState=followLine;
-    changeState=false;
+  if(changeState==true){
+      if(detected==centerOfLine && interval>70){
+        currentState=followLine;
+        changeState=false;
+      }else
+      {
+        currentTime=millis();
+        interval=currentTime-startTime;
+        movement=moveOnLine;
+      }
   }
-
   break;
 
   case reachedEventLocation:
+    //this states is when the bot reaches the nearest node from the event
+    //We check the stopCondition and use it to stop the bot
     movement=moveOnLine;
     stopIf = stopCondition(currentNode);
+    currentTime=millis();
+    interval=currentTime-startTime;
 
-    if(detected==stopIf && currentNode != 4) {
+    if(detected==stopIf && currentNode != 4 && interval>400) {
       movement=stop;
       currentState=stopAtEvent;
+      digitalWrite(BUZZER, LOW);
       startTime=millis();
 
     }
 
     if(detected==stopIf && currentNode == 4 ) {
-      currentTime=millis();
-      interval=currentTime-startTime;
-      if(interval>7000) {
+      if(interval>3500) {
         movement=stop;
         currentState=stopAtEvent;
+        digitalWrite(BUZZER, LOW);
         startTime=millis();
         }
     }
@@ -480,19 +484,24 @@ void loop() {
   break;
 
   case stopAtEvent:
+    //stop the bot for desired time of 1sec and checks if it hass to go forward or turnaround
     currentTime=millis();
     interval=currentTime-startTime;
     isBotReached=false;
-    if(interval>=3000){
+    if(interval>=1000){
+      digitalWrite(BUZZER, HIGH);
       pathIndex++;
       if(currentNode == currentPath[pathIndex]) {
       prevNode=nextNode;
       currentNode=nextNode;
       nextNode=currentPath[pathIndex];
       pathIndex++;
-      currentState = turningAround;
+        if(stopIf == centerOfLine || currentNode == 9){
+        currentState=turningAroundLine;
+        } else {
+          currentState = turningAround;
+        }
       movement = aboutTurn;
-      // pathIndex=1;
       startTime=millis();
       } else {
       pathIndex++;
@@ -502,41 +511,57 @@ void loop() {
   break;
 
   case turningAround :
-    // currentTime=millis();
-
-    if(stopIf == centerOfLine || currentNode==9){
-      currentState=turningAroundLine;
-    }
-
-    if(detected==sideRightLine && sensorActiveCount==0) {
+    // turning around in places of 3 lines
+    if(sideRightSensor && sensorActiveCount==0) {
       sensorActiveCount++;
     }
 
-    // interval=currentTime-startTime;
-    if(detected==centerOfLine && sensorActiveCount!=0){
+    if(sideLeftSensor && sensorActiveCount==1){
+      sensorActiveCount++;
+    }
+
+    if(leftSensor && sensorActiveCount==2){
       movement=moveOnLine;
       sensorActiveCount=0;
       currentState=followLine;
-      // lastIndex=0;
     }
-
   break;
 
   case turningAroundLine :
+  //turniong around in places of two sidelines
+    currentTime=millis();
+    interval=currentTime-startTime;
 
-    if(detected==sideLeftLine && sensorActiveCount==0){
+    if(interval<600 && currentNode == 9){
+      sensorActiveCount=9;
+      movement=moveOnLine;
+    } 
+    else if(interval>600 && sensorActiveCount == 9){
+        sensorActiveCount=0;
+        movement=aboutTurn;
+    }
+
+    if(sideLeftSensor && sensorActiveCount==0){
       sensorActiveCount++;
     }
 
-    if(detected==leftOfLine && sensorActiveCount==1){
+    if(leftSensor && sensorActiveCount==1){
       sensorActiveCount++;
     }
 
-    if(detected==sideLeftLine && sensorActiveCount==2){
+    if(rightSensor && sensorActiveCount==2){
       sensorActiveCount++;
     }
 
-    if(detected==sideRightLine && sensorActiveCount==3){
+    if(sideRightSensor && sensorActiveCount==3){
+      sensorActiveCount++;
+    }
+
+    if(leftSensor && sensorActiveCount==4){
+      sensorActiveCount++;
+    }
+
+    if(sideRightSensor && sensorActiveCount==5){
       movement=moveOnLine;
       sensorActiveCount=0;
       currentState=followLine;
@@ -546,48 +571,50 @@ void loop() {
 
   case endRun :
     movement=stop;
-    if(pathIndex==lastIndex && detected==allThreeLine){
-      // If the end of the path is reached, stop the bot
-      digitalWrite(BUZZER, LOW);
-      digitalWrite(LED_RED, HIGH);
+      // this stops the bot and play the buzzer for 5 secs.
       delay(5000);
       digitalWrite(BUZZER, HIGH);
       digitalWrite(LED_RED, LOW);
-      pathIndex++;
-    }
     break;
 
- 
  }
 
-  switch (movement) {
-    case forward:
-      moveForward(FULL_SPEED, FULL_SPEED,0);
-      break;
+    //this is used to control the motors based on the value of movement from the state machine
+    switch (movement){
+        case forward:
+          moveForwardPWM(FULL_SPEED,STOP_SPEED,FULL_SPEED,STOP_SPEED);
+          break;
 
-    case left:
-      moveForward(STOP_SPEED, TURN_SPEED,0);
-      break;
-    
-    case eLeft:
-      moveForward(STOP_SPEED, EXTREME_TURN_SPEED,0);
-      break;
+        case left:
+          moveForwardPWM(FULL_SPEED,STOP_SPEED,TURN_SPEED,STOP_SPEED);
+          break;
 
-    case right:
-      moveForward(TURN_SPEED, STOP_SPEED,0);
+        case right:
+          moveForwardPWM(TURN_SPEED,STOP_SPEED,FULL_SPEED,STOP_SPEED);
+          break;
+        
+        case eLeft:
+          moveForwardPWM(STOP_SPEED,EXTREME_TURN_SPEED,EXTREME_TURN_SPEED+20,STOP_SPEED);
+        break;
+        
+        case eRight:
+          moveForwardPWM(EXTREME_TURN_SPEED,STOP_SPEED,STOP_SPEED,EXTREME_TURN_SPEED-20);
+          break; 
 
-      break;
-    
-    case eRight:
-      moveForward(EXTREME_TURN_SPEED, STOP_SPEED,0);
-      break;
+        case sideLeft:
+          moveForwardPWM(STOP_SPEED,TURN_SPEED,TURN_SPEED-30,STOP_SPEED); 
+          break;
 
-    case stop:
-      moveForward(STOP_SPEED, STOP_SPEED,0);
-      break;
+        case sideRight:
+           moveForwardPWM(TURN_SPEED-30,STOP_SPEED,STOP_SPEED,TURN_SPEED);
+           break;                                                                                     
 
-    case aboutTurn:
-      moveForward(0,TURN_SPEED,TURN_SPEED);
-      break;
-  }
+        case stop:
+          moveForwardPWM(STOP_SPEED,STOP_SPEED,STOP_SPEED,STOP_SPEED);
+          break;
+
+        case aboutTurn:
+          moveForwardPWM(STOP_SPEED,TURN_SPEED-20,TURN_SPEED-20,STOP_SPEED);
+          break;
+    }
 }
